b2fe1888536297413d6e3d244985ce47c863ce28
IzantMcoronal <izant2020@gmail.com>
2020-11-10 18:26:21 +0100

    1 file modified

--- script.js
+++ script.js
@@ -17,15 +17,23 @@ function onLoad() {
 }
 
 function getAll (){
+  
+  //Creamos la variable indicador para situarnos en el Li de la lista
   let indicador = document.querySelector(".selected").parentElement.parentElement.childNodes;
-
+  
+  //Creamos una variable contador
   let contador = 1;
 
+  //Realizamos un for de los Li almacenados en indicador
   for (var i = 0; i < indicador.length; i++){
+    
+    //Si al recorrer los Li encuentra un elementoHTML lo guarda
     if( indicador [i] instanceof HTMLElement){
-
+      
+      //creamos la variable Li que nos lleva a los hijos span
       var li = indicador[i].children;
-
+      
+      //le añadimos la clase "element + su numero" a cada span
       li[0].classList.add("element-" + contador);
       contador++;
 
@@ -34,8 +42,10 @@ function getAll (){
 }
 
 function deleteImp(){
+  
   //Creamos una variable donde metemos todos los Li
   var deleteList = document.querySelectorAll("Li");
+  
   //Borramos los li seleccionados que son los li pares 2 y 4
   deleteList[1].remove();
   deleteList[3].remove();
@@ -45,10 +55,13 @@ function addLi() {
 
   //Seleccionamos el elemento "List2" por su Id
   var listNode = document.getElementById("list2"),
+  
   //Creamos el boton
   var button1 = document.createElement("Button");
+      
       //le agregamos el texto del boton
       button1.innerHTML = "Texto ejemplo 1";
+      
       //le agregamos la clase al boton
       button1.classList.add("element-1");
 
@@ -60,8 +73,10 @@ function addLi() {
       button5.innerHTML = "Texto ejemplo 5";
       button5.classList.add("element-5");
       button5.disabled=true;
+  
   //creamos la Lista
   var li1 = document.createElement("LI");
+     
       //agregamos a la lista un hijo que sera el boton
       li1.appendChild(button1);
 
@@ -70,8 +85,10 @@ function addLi() {
 
   var li5 = document.createElement("Li");
       li5.appendChild(button5);
+      
       //Con este comando desactivamos el boton 5.
       button5.disabled;
+ 
   //agregamos la lista con el boton a "lista2"
   listNode.appendChild(li1);
   listNode.appendChild(li3);
