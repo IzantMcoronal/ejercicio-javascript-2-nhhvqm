cda54ffc23abb0712b187042b4c10a0edf065dab
IzantMcoronal <izant2020@gmail.com>
2020-11-10 14:36:41 +0100

    1 file modified

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 //    * Añadid la clase "element-n" a cada "span" de la lista, siendo "n" el número del lugar que ocupa en la lista (por ejemplo el segundo "span" de la lista tendría la clase "element-2"). Para ello, haced uso de los selectores de nodo (parentNode, nextSibling, firstChild...) partiendo del "span" con la clase "selected" siempre.
 //    * Tras añadir las clases, haced uso de "querySelectorAll" para obtener solo los elementos "li" con valor par (teniendo en cuenta que el primer elmento es el 1) y, a continuación, eliminadlos.
 
-
 //En la segunda lista, que en principio está vacía, deberéis hacer esto:
 //    * Tenéis que generar dentro de esta lista, nodo a nodo, la misma estructura que ha quedado en la primera lista en la que realizastéis los cambios. Tiene que quedar igual, con la misma jerarquía y con las mismas clases, pero con la diferencia de que en vez de elementos "span" deben ser botones. Para replicar los elementos de la primera lista no hace falta que la recorráis, podéis simplemente generar cada elemento "a mano" una detrás de otro (aunque si usáis la lista de referencia para hacer algún tipo de bucle, mejor)
 //    * Después de generar esta segunda lista, añadid el atributo disabled al último botón.
@@ -12,8 +11,10 @@
 window.addEventListener('load', onLoad);
 
 function onLoad() {
-  console.log('hi');
-
   const span = document.querySelector('.selected');
-  console.log(span);
+  getPadre = span.parentNode;
+  getPadrepadre=getPadre.previousSibling;
+  getPadrepadrepadre = getPadrepadre.previousSibling;
+  console.log(getPadrepadrepadre)
+  
 }
